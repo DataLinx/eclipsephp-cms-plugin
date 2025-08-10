3567d7b6cf60c3790fc576a81e41f7c1744c7e62
ankitcodes4u <ankitcodes4u@gmail.com>
2025-08-11 05:09:57 +0545

    refactor: remove redundant code and standardize test naming

--- src/CmsPlugin.php
+++ src/CmsPlugin.php
@@ -3,12 +3,8 @@
 namespace Eclipse\Cms;
 
 use Eclipse\Common\Foundation\Plugins\Plugin;
-use Filament\Panel;
 
 class CmsPlugin extends Plugin
 {
-    public function register(Panel $panel): void
-    {
-        parent::register($panel);
-    }
+    //
 }
